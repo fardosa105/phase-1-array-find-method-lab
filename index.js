671b89bf72ec620ba29019267985de47fe5b826b
+ <fadosa.abdirhman@student.moringaschool>
2024-07-06 11:11:23 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+// index.js
+
+function superbowlWin(record) {
+    const winRecord = record.find(obj => obj.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  // Export the function if needed for tests
+  module.exports = superbowlWin;
+  
+  
